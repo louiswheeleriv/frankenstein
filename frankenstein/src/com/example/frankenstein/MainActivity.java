b62b5985fabdf2dda1913c3fd0f104c8466b0233
Louis Wheeler <wheelela@lafayette.edu>
2014-10-07 16:15:02 -0400

    Added SearchObject class and prepared MainActivity for connection to DB.

--- frankenstein/src/com/example/frankenstein/MainActivity.java
+++ frankenstein/src/com/example/frankenstein/MainActivity.java
@@ -1,21 +1,32 @@
 package com.example.frankenstein;
 
-import android.support.v7.app.ActionBarActivity;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 /**
  * @author Louis Wheeler
  */
 
-public class MainActivity extends Activity{
+public class MainActivity extends Activity implements OnClickListener{
 
+	private final String SERVER_ADDRESS = "tomcat.cs.lafayette.edu";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		//setContentView(R.layout.activity_main);
 	}
 	
 	@Override
@@ -27,7 +38,7 @@ public class MainActivity extends Activity{
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		//getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
@@ -37,9 +48,38 @@ public class MainActivity extends Activity{
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
+		/*
 		if (id == R.id.action_settings) {
 			return true;
 		}
+		*/
 		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onClick(View v) {
+		try {
+			URL url = new URL(SERVER_ADDRESS);
+			URLConnection connection = url.openConnection();
+			
+			String inputString = "";
+			
+			connection.setDoOutput(true);
+			OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
+			out.write(inputString);
+			out.close();
+			
+			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			String returnString = "";
+			
+			while((returnString = in.readLine()) != null){
+				
+			}
+			
+			in.close();
+			
+		}catch(Exception e){
+			Log.d("Exception", e.toString());
+		}
 	}
 }
