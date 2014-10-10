0fda025970a578709d134cc82af8fe6606860505
Louis Wheeler <wheelela@lafayette.edu>
2014-10-10 13:10:46 -0400

    Restarted project, cleaned things up and added second activity

---
+++ FrankensteinForAndroid/src/edu/cs320/frankensteinforandroid/SearchActivity.java
@@ -0,0 +1,76 @@
+package edu.cs320.frankensteinforandroid;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.os.Build;
+
+public class SearchActivity extends ActionBarActivity {
+
+	public final static String EXTRA_INPUT = "edu.cs320.frankensteinforandroid.INPUT";
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_search);
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.search, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+			
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_search,
+					container, false);
+			return rootView;
+		}
+	}
+	
+	public void searchButtonClicked(View view){
+		
+		Intent intent = new Intent(this, ResultActivity.class);
+		EditText editText = (EditText) findViewById(R.id.edit_searchValue);
+		String searchValue = editText.getText().toString();
+		intent.putExtra(EXTRA_INPUT, searchValue);
+		startActivity(intent);
+		
+	}
+}
