8f8070ee12baa1dff35ba6e89aafb443ace8966c
machira <raymond.machira@gmail.com>
2014-10-17 13:13:48 -0400

    creating tests specific to our postgress db

--- frankenstein_mobile/api/models.py
+++ frankenstein_mobile/api/models.py
@@ -1,55 +1,86 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+#
+# Also note: You'll have to insert the output of 'django-admin.py sqlcustom [app_label]'
+# into your database.
+from __future__ import unicode_literals
+
 from django.db import models
 
 
-class Production(models.Model):
-    production_name = models.CharField(max_length=200)
-    production_info = models.CharField(max_length=200)
-
-    def __unicode__(self):
-        return "Production: {0}, Description: {1}".format(self.production_name, self.production_info)
-
-
-class Stage(models.Model):
-    stage_location = models.CharField(max_length=200)
-    stage_description = models.CharField(max_length=200)
-
-    def __unicode__(self):
-        return "Location: {0}, Description: {1}".format(self.stage_location, self.stage_description)
-
-
 class Actor(models.Model):
-    actor_name = models.CharField(max_length=200)
-    actor_bio = models.CharField(max_length=200)
+    actorid = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50, blank=True)
+    bio = models.TextField(blank=True)
 
-    def __unicode__(self):
-        return "Name: {0}, Bio: {1}".format(self.actor_name, self.actor_bio)
-
-
-class CrewResponsibility(models.Model):
-    responsibility = models.CharField(max_length=200)
-
-    def __unicode__(self):
-        return "Responsibility: {0}".format(self.responsibility)
+    class Meta:
+        managed = False
+        db_table = 'actor'
 
 
 class Crew(models.Model):
-    crew_name = models.CharField(max_length=200)
-    crew_responsibility = models.ForeignKey(CrewResponsibility)
-    crew_bio = models.CharField(max_length=200)
+    crewid = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50, blank=True)
+    responsiblities = models.CharField(max_length=50, blank=True)
+    bio = models.TextField(blank=True)
 
-    def __unicode__(self):
-            return "Name: {0}, Bio: {1}".format(self.crew_name, self.crew_bio)
+    class Meta:
+        managed = False
+        db_table = 'crew'
+
+
+class Perfactor(models.Model):
+    performanceid = models.ForeignKey('Performance', db_column='performanceid', blank=True, null=True)
+    actorid = models.ForeignKey(Actor, db_column='actorid', blank=True, null=True)
+    appearancetime = models.TimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'perfactor'
+
+
+class Perfcrew(models.Model):
+    performanceid = models.ForeignKey('Performance', db_column='performanceid', blank=True, null=True)
+    crewid = models.ForeignKey(Crew, db_column='crewid', blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'perfcrew'
 
 
 class Performance(models.Model):
-    performance_stage = models.ForeignKey(Stage)
-    performance_info = models.CharField(max_length=200)
-    performance_start_time = models.DateTimeField()
-    performance_production = models.ForeignKey(Production)
-    performance_actors = models.ManyToManyField(Actor)
-    performance_crews = models.ManyToManyField(Crew)
+    performanceid = models.IntegerField(primary_key=True)
+    info = models.TextField(blank=True)
+    stageid = models.ForeignKey('Stage', db_column='stageid', blank=True, null=True)
+    starttime = models.TimeField(blank=True, null=True)
+    productionid = models.ForeignKey('Production', db_column='productionid', blank=True, null=True)
+    actors = models.ManyToManyField(Actor, through='Perfactor',through_fields=( 'performanceid','actorid'))
+    crews = models.ManyToManyField(Crew, through='Perfcrew', through_fields=('performanceid','crewid'))
 
-    def __unicode__(self):
-            return 'Production: {0}, Stage: {1}, Start Time: {2}, Info: {3}, Actor: {4}, Crew: {5}'.format(self.performance_production, self.performance_stage,
-                                                  self.performance_start_time, self.performance_info,
-                                                  '', '')
+    class Meta:
+        managed = False
+        db_table = 'performance'
+
+
+class Production(models.Model):
+    productionid = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50, blank=True)
+    info = models.TextField(blank=True)
+
+    class Meta:
+        managed = False
+        db_table = 'production'
+
+
+class Stage(models.Model):
+    stageid = models.IntegerField(primary_key=True)
+    location = models.CharField(max_length=50, blank=True)
+    description = models.TextField(blank=True)
+
+    class Meta:
+        managed = False
+        db_table = 'stage'
--- frankenstein_mobile/frankenstein_site/admin.py
+++ frankenstein_mobile/frankenstein_site/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from api.models import Performance, Actor, Stage, Crew, Production, CrewResponsibility
+from api.models import Performance, Actor, Stage, Crew, Production #, CrewResponsibility
 
 admin.site.register(Production)
 admin.site.register(Performance)
 admin.site.register(Actor)
 admin.site.register(Stage)
 admin.site.register(Crew)
-admin.site.register(CrewResponsibility)
+# admin.site.register(CrewResponsibility)
 # Register your models here.
