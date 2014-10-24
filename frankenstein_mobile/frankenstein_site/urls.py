1f90b83dffb55648807718aece83f2b8f425a1b2
Ellen Galperin <galperie@lafayette.edu>
2014-10-23 23:20:43 -0400

    Most of the front end searching is working and the searches are showing

--- frankenstein_mobile/frankenstein_site/urls.py
+++ frankenstein_mobile/frankenstein_site/urls.py
@@ -17,14 +17,29 @@ urlpatterns = patterns('',
     # ex: /actor/
     # url(r'^search/$', views.search, name='search'),
     url(r'^search_actor/$', views.search_actor, name='search_actor'),
-    url(r'^search_stage/$', views.search_stage, name='search_stage'),
+    url(r'^search_actor/results_actor/$', views.results_actor, name='results_actor'),
+
     url(r'^search_crew/$', views.search_crew, name='search_crew'),
-    url(r'^search_performance/$', views.search_performance, name='search_performance'),
+    url(r'^search_crew/results_crew/$', views.results_crew, name='results_crew'),
+
+    url(r'^search_stage/$', views.search_stage, name='search_stage'),
+    url(r'^search_stage/results_stage/$', views.results_stage, name='results_stage'),
+
     url(r'^search_time/$', views.search_time, name='search_time'),
+    url(r'^search_time/results_time/$', views.results_time, name='results_time'),
+
+
+    url(r'^results$', views.results_stage, name='results_stage'),
+
+
+
+
+    url(r'^search_performance/$', views.search_performance, name='search_performance'),
+
 
     # ex: /actor/results/
     # url(r'^(?P<question_id>\w+)/results/$', views.results, name='results'),
-     url(r'^search_actor/results/$', views.results, name='results'),
+
 
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
