f9276a88a5ef0999f746db1074e6584fb4fd61ec
Ellen Galperin <galperie@lafayette.edu>
2014-11-12 20:42:20 -0500

    Node is set up in the folder Frankenstein_admin.
    A basic page is up and should link to port 3100

--- Frankenstein_admin/admin_page/app.js
+++ Frankenstein_admin/admin_page/app.js
@@ -15,9 +15,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// port
-app.set('port', process.env.PORT || 37715);
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
