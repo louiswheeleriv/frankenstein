e1e34120942e2b222c41e8a087f06fe4ca0397bb
Ellen Galperin <galperie@lafayette.edu>
2014-11-17 21:04:24 -0500

    Edited the front page to have some design and added a form for louis to work with

--- Frankenstein_admin/admin_page/routes/index.js
+++ Frankenstein_admin/admin_page/routes/index.js
@@ -11,4 +11,13 @@ router.get('/helloworld', function(req, res) {
 	res.render('helloworld', {title: 'Hello, World!', name:'Frankenstein' })
 });
 
+
+// HERE LOUIS IS WHERE THE POST INFORMATION GOES
+router.post('/add_actor', function(req, res) {
+	console.log(req.body);
+	console.log(req.body.actor_name);
+	console.log(req.body.actor_bio);
+	res.redirect('/');
+})
+
 module.exports = router;
