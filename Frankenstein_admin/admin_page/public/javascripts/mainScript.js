d0d7d00a974c4c5a486c6e34a97d172ac67fb064
Ellen Galperin <galperie@lafayette.edu>
2014-11-21 01:13:42 -0500

    changed ui structure

---
+++ Frankenstein_admin/admin_page/public/javascripts/mainScript.js
@@ -0,0 +1,30 @@
+//file : hello.js
+$(document).ready(function() {
+	console.log("hello world");
+
+	
+});
+
+ 
+// function sayHelloToThisPerson(name){
+// 	alert('hello : ' + name);
+// }
+// //run above js function when this js is loaded
+// sayHelloToThisPerson('winnie');
+
+// console.log('am I working?????');
+
+// $(document).ready(function() {
+//     $("#btn1").on('click', function() {
+//         $.post("jq.php",{"n" : $("#v").val()}, function(d,s) {
+
+//             var ul = $('<ul />');
+
+//             $(d).children().each(function(i,e) {
+//                 ul.append("<li>"+$(e).text()+"</li>");
+//             });
+
+//             $("#div1").html(ul);
+//         });
+//     });
+// });
